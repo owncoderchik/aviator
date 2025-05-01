5a785d2aac60d071029b38fb5d2fc5a1fa7afca2
owncoderchik <askorbinka.tt@gmail.com>
2025-05-01 18:55:20 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ function goTimerError(time) {
 }
 
 getSignal.onclick = function () {
-    let receivingSignal = getRandomFloat(3.21, 3.21, 2);
+    let receivingSignal = getRandomFloat(2.61, 2.61, 2);
     if (receivingSignal.toString().length == 3) {
         receivingSignal += "0";
     }
